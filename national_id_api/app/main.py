ef0a5249b4b1da897bfa5872e56fe8a8c0116076
MohanadGadallah <mohanad.gad38@gmail.com>
2025-08-16 21:14:02 +0300

    test: commit message

--- national_id_api/app/main.py
+++ national_id_api/app/main.py
@@ -4,7 +4,6 @@ import logging
 from fastapi import FastAPI, Request, status, Header, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
-from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from slowapi import Limiter
 from slowapi.util import get_remote_address
